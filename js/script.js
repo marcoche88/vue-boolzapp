f5dba26806d771946e5f1ed8ec3c628d751b7abe
marcoche88 <marco.che@hotmail.it>
2021-08-27 16:23:48 +0200

    added input message in chat

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 Vue.config.devtools = true;
 
+// plugin dayjs
+dayjs.extend(dayjs_plugin_customParseFormat);
+
+// locale dayjs
+dayjs.locale("it");
+
 const app = new Vue({
     el: "#root",
     data: {
         user: data.user,
         contacts: data.contacts,
         activeNumber: 0,
+        inputMessage: "",
     },
     methods: {
         selectContact(i) {
             this.activeNumber = i;
         },
+        addMessage() {
+            const currentChat = this.contacts[this.activeNumber].messages;
+            currentChat.push({
+                date: dayjs().format("DD/MM/YYYY HH:mm:ss"),
+                message: this.inputMessage,
+                status: 'sent',
+            });
+            this.inputMessage = "";
+        }
     },
 });
